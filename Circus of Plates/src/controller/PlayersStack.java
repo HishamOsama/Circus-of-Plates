16537da081a5b251ea570a7f47d939b34e97d419
Muhammed Essam Khamis <Muhammed@Muhammed-PC>
2017-01-17 17:03:45 +0200

    Done : 		-Back end of Player Stacks added. To be done : 		- Front end of the Player Stacks. 		- Back end of the ScoreManager. Notes : 		- There is some notes on shape class, some new attr. needs 		  to be put in order to help the PlayerStack class.

---
+++ Circus of Plates/src/controller/PlayersStack.java
@@ -0,0 +1,102 @@
+package controller;
+
+import java.util.Stack;
+
+/**
+ *  General Comments :
+ *              - The blue comments are used to documentation.
+ *              - The green comments indicates about something want
+ *                  to be add to the Class.
+ *  ---------------------------------------------------------------
+ *                  
+ *  This class are used to control the stack of the players,
+ *  and notify the ScoreManager to increase the score.
+ *  
+ * 
+ * */
+
+public class PlayersStack {
+    
+    /**
+     *  Enumeration to differ between stacks of players
+     */
+    public enum players{
+        player1,player2;
+    }
+    /**
+     *  Max number of similarity we want to increase the score.
+     * */
+    private final int similarity = 3;
+    /**
+     *  Stack that save the names of the shapes that should be distinct.
+     * */
+    private Stack<String> plates;
+    /**
+     *  Which player this object belongs to.
+     * */
+    private players type;
+    /**
+     *  Score controller that should be modified if we found match.
+     * */
+    //private score controller here;
+    
+    public PlayersStack(players type/*, Score controller*/) {
+        // TODO Auto-generated constructor stub
+        plates = new Stack<>();
+        this.type = type;
+        // this.scoreController = scoreController;
+    }
+    /**
+    *   Add the new shape to the stack ,then check if we found match
+    *   or not.
+    *       @param plate
+    *                   the plate to put in the Stack. 
+    */
+    public void addShape(String plate){
+        plates.push(plate);
+        checkStack();
+        
+    }
+    /**
+     *  This method is used to put the items we want to check about them
+     *      in an array.
+     *    
+     * */
+    private void checkStack(){
+        if(plates.size() >= similarity){
+            String[] platesToCheck = new String[similarity];
+            for(int i = 0 ; i < similarity ; i++){
+                platesToCheck[i] = plates.pop(); 
+            }
+            boolean allSimilar = checkSimilarity(platesToCheck);
+            if(allSimilar){
+                updateScore();
+            }
+        }
+    }
+    /**
+    * This method is used to see it the items in the array are similar
+    *   or not 
+    *       @param platesToCheck
+    *                       array we want to compare its items
+    *       @return true -> the items are all similar
+    *               false -> at least one item is not similar 
+    */
+    private boolean checkSimilarity (String[] platesToCheck){
+        boolean allSimilar = true;
+        String comparator = platesToCheck[0];
+        for(int i = similarity-1 ; i > 0 ; i--){
+            if(!platesToCheck[i].equals(comparator)){
+                allSimilar = false;
+            }
+            plates.push(platesToCheck[i]);
+        }
+        return allSimilar;
+    }
+    /**
+     *  modify the Score manager to update the score of the current player.
+     * */
+    private void updateScore(){
+        // updateScoreManager(type);
+    }
+}
