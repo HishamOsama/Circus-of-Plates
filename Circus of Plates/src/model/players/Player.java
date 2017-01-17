b3b00659875ce88c139939bbdbb3af7750e19632
Muhammed Essam Khamis <Muhammed@Muhammed-PC>
2017-01-17 21:42:03 +0200

    Different clowns

--- Circus of Plates/src/model/players/Player.java
+++ Circus of Plates/src/model/players/Player.java
@@ -1,18 +1,34 @@
 package model.players;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player {
 
-	private final String imagePath;
-	private final File image;
+    private final String imagePath;
+    private File imageFile;
+    private BufferedImage image;
 
-	public Player(String imageName) {
-		imagePath = "C:\\Users\\Hesham\\git\\circus-of-plates\\Circus of Plates\\" + "ClownImage" + File.separator + imageName;
-		image = new File(imagePath);
-	}
+    public Player(String imageName) {
+        imagePath = "C:\\Users\\Hesham\\git\\circus-of-plates\\Circus of Plates\\" + "ClownImage" + File.separator
+                + imageName;
+        loadImage();
+    }
 
-	public File getImage() {
-		return image;
-	}
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    private void loadImage() {
+        imageFile = new File(imagePath);
+        try {
+            image = ImageIO.read(imageFile);
+            System.out.println("Image Loaded Successfully");
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
