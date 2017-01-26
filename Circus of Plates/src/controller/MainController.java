13287a7fb011c040392f7ec9b57fd84c8df58a9d
Ahmed Naggar <ahmednagar2011@gmail.com>
2017-01-26 06:04:31 +0200

    Done

--- Circus of Plates/src/controller/MainController.java
+++ Circus of Plates/src/controller/MainController.java
@@ -2,21 +2,23 @@ package controller;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 
 import controller.util.Enumrations.Players;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.util.Duration;
 import model.gamestates.GameState;
 import model.gamestates.PausedState;
@@ -25,7 +27,12 @@ import model.gamestates.Player2WinsState;
 import model.gamestates.TiedState;
 import model.players.AbstractPlayer;
 import model.save.PlayersStacksData;
+import model.save.SaveShapeNode;
+import model.save.SavedStates;
+import model.save.Snapshot;
 import model.shapes.ShapesMovements;
+import model.shapes.interfaces.Shape;
+import model.shapes.util.PlatesFactory;
 import util.DimensionsConstants;
 
 public class MainController {
@@ -63,6 +70,8 @@ public class MainController {
 	private int difficulty;
 	private ImageView pauseImage;
 	private ImageView saveImage;
+	private ImageView player1Imgae;
+	private ImageView player2Imgae;
 
 	@FXML
 	public void initialize() {
@@ -77,7 +86,8 @@ public class MainController {
 		// Load Players
 		final ImageView player1 = createP1();
 		final ImageView player2 = createP2();
-
+		player1Imgae = player1;
+		player2Imgae = player2;
 		// Make Players Movable
 		move(paneFXid, player1, player2);
 
@@ -94,7 +104,7 @@ public class MainController {
 		String backgroundPath = System.getProperty("user.dir") + File.separator + "Resources" + File.separator
 				+ "wallpaper.jpg" + File.separator;
 		backgroundPath = new File(backgroundPath).toURI().toString();
-		Image backgroundImage = new Image(backgroundPath);
+		final Image backgroundImage = new Image(backgroundPath);
 		imageView.setImage(backgroundImage);
 		imageView.setFitWidth(DimensionsConstants.XBoundary);
 		imageView.setFitHeight(DimensionsConstants.YBoundary);
@@ -103,17 +113,17 @@ public class MainController {
 		String shelfPath = System.getProperty("user.dir") + File.separator + "Resources" + File.separator + "shelf.png"
 				+ File.separator;
 		shelfPath = new File(shelfPath).toURI().toString();
-		Image shelfImage = new Image(shelfPath);
+		final Image shelfImage = new Image(shelfPath);
 
 		// Left Shelf...
-		ImageView leftShelf = new ImageView(shelfImage);
+		final ImageView leftShelf = new ImageView(shelfImage);
 		leftShelf.setX(-50);
 		leftShelf.setY(-120);
 		leftShelf.setFitWidth(470);
 		paneFXid.getChildren().add(leftShelf);
 
 		// Right Shelf...
-		ImageView RightShelf = new ImageView(shelfImage);
+		final ImageView RightShelf = new ImageView(shelfImage);
 		RightShelf.setX(830);
 		RightShelf.setY(-120);
 		RightShelf.setFitWidth(420);
@@ -124,7 +134,7 @@ public class MainController {
 		String pausePath = System.getProperty("user.dir") + File.separator + "Resources" + File.separator + "pause.png"
 				+ File.separator;
 		pausePath = new File(pausePath).toURI().toString();
-		Image pause = new Image(pausePath);
+		final Image pause = new Image(pausePath);
 
 		pauseImage = new ImageView(pause);
 		pauseImage.setX(0);
@@ -137,7 +147,7 @@ public class MainController {
 		String savePath = System.getProperty("user.dir") + File.separator + "Resources" + File.separator + "save.png"
 				+ File.separator;
 		savePath = new File(savePath).toURI().toString();
-		Image save = new Image(savePath);
+		final Image save = new Image(savePath);
 
 		saveImage = new ImageView(save);
 		saveImage.setX(50);
@@ -155,7 +165,7 @@ public class MainController {
 		pauseImage.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 
 			@Override
-			public void handle(MouseEvent event) {
+			public void handle(final MouseEvent event) {
 				Paused.changeState();
 			}
 		});
@@ -166,23 +176,41 @@ public class MainController {
 		saveImage.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 
 			@Override
-			public void handle(MouseEvent event) {
+			public void handle(final MouseEvent event) {
 
-				// LOGIC HERE...
-				int len = Players.values().length;
-				int[] scores = new int[len];
-				for (Players t : Players.values()) {
+				Paused.changeState();
+
+				// File Chooser
+				final FileChooser fileChooser = new FileChooser();
+
+				// Set extension filter
+				final FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+
+
+				fileChooser.getExtensionFilters().addAll(extFilter);
+
+				// Show save file dialog
+				final File path = fileChooser.showSaveDialog(null);
+				final String pathString = path.toString();
+				final File f = new File(pathString);
+				final String fileName = f.getName().substring(0, f.getName().indexOf('.'));
+				final String absoulutePath = (pathString.substring(0, pathString.indexOf(fileName)));
+				// Getting Scores
+				final int len = Players.values().length;
+				final int[] scores = new int[len];
+				for (final Players t : Players.values()) {
 					scores[t.ordinal()] = scoreManager.getScore(t);
 				}
-				// GameState g = new GameState(scores, countingNumbers,
-				// difficulty);
-				PlayersStacksData[] data = new PlayersStacksData[len];
-				AbstractPlayer p1 = resourcesManager.getFirstPlayer();
-				AbstractPlayer p2 = resourcesManager.getSecondPlayer();
+				// Getting Players for positions and StackLists
+				final AbstractPlayer p1 = resourcesManager.getFirstPlayer();
+				final AbstractPlayer p2 = resourcesManager.getSecondPlayer();
+				final SavedStates savedStates = new SavedStates(scores, countingNumbers,
+				 difficulty, p1.getPlayerPosition()[0], p2.getPlayerPosition()[0]);
+				final PlayersStacksData[] data = new PlayersStacksData[len];
 				data[0] = new PlayersStacksData(p1.getStackList(0), p1.getStackList(1));
 				data[1] = new PlayersStacksData(p2.getStackList(0), p2.getStackList(1));
-				// Snapshot save = new Snapshot(g, data);
-				// save.saveShot(System.getProperty("user.dir"), "test");
+				final Snapshot save = new Snapshot(savedStates, data);
+				save.saveShot(absoulutePath, fileName);
 				// Snapshot load = new Snapshot();
 				// load.LoadDate(System.getProperty("user.dir"), "test");
 
@@ -219,6 +247,21 @@ public class MainController {
 		return imageView;
 	}
 
+	public AbstractPlayer getPlayer1() {
+		return player1;
+	}
+
+	public AbstractPlayer getPlayer2() {
+		return player2;
+	}
+
+	public void setTimeAfterLoad(final int elapsedTime) {
+		countingNumbers = elapsedTime;
+	}
+
+	public ScoreManager getScoreManager() {
+		return scoreManager;
+	}
 	private void move(final Pane pane, final ImageView player1Image, final ImageView player2Image) {
 		final PlayersMovement playersMovement = new PlayersMovement(pane, resourcesManager, player1Image, player2Image);
 		playersMovement.start();
@@ -305,7 +348,7 @@ public class MainController {
 
 					pauseLabel.setText("");
 				} else {
-					GameState state = new PausedState();
+					final GameState state = new PausedState();
 					pauseLabel.setText(state.printProperMessage());
 				}
 			}
@@ -318,15 +361,15 @@ public class MainController {
 	private void terminate() {
 		paneFXid.getChildren().clear();
 		paneFXid.getChildren().add(imageView);
-		Label l = new Label();
+		final Label l = new Label();
 		l.setText("");
 		l.setFont(new Font(60));
 		l.setLayoutX(480);
 		l.setLayoutY(35);
 		paneFXid.getChildren().add(l);
 
-		int x = scoreManager.getScore(Players.player1);
-		int y = scoreManager.getScore(Players.player2);
+		final int x = scoreManager.getScore(Players.player1);
+		final int y = scoreManager.getScore(Players.player2);
 
 		GameState state;
 		if (x > y) {
@@ -354,4 +397,53 @@ public class MainController {
 
 		return dispaly;
 	}
+
+	public void loadStates(final SavedStates savedStates, final PlayersStacksData[] data){
+		setTimeAfterLoad(savedStates.getElapsedTime());
+		player1Imgae.setX(savedStates.getP(0)[0]);
+		player1Imgae.setY(savedStates.getP(0)[1]);
+		player2Imgae.setX(savedStates.getP(1)[0]);
+		player2Imgae.setY(savedStates.getP(1)[1]);
+		fetchLoadedPlates(data);
+	}
+
+	private void fetchLoadedPlates(final PlayersStacksData[] data) {
+		ArrayList<SaveShapeNode> savedPlates = data[0].getlefttHandshapes();
+		for (final SaveShapeNode savedPlate : savedPlates) {
+			final Shape plate = PlatesFactory.getInstance().getShape(savedPlate.getName());
+			final ImageView imageView = convertImage(plate.getImage());
+			imageView.setFitHeight(DimensionsConstants.IMAGE_SIZE);
+            imageView.setFitWidth(DimensionsConstants.IMAGE_SIZE);
+			paneFXid.getChildren().add(imageView);
+			player1.receivePlate(1, plate, imageView);
+		}
+		savedPlates = data[0].getRightHandshapes();
+		for (final SaveShapeNode savedPlate : savedPlates) {
+			final Shape plate = PlatesFactory.getInstance().getShape(savedPlate.getName());
+			final ImageView imageView = convertImage(plate.getImage());
+			imageView.setFitHeight(DimensionsConstants.IMAGE_SIZE);
+            imageView.setFitWidth(DimensionsConstants.IMAGE_SIZE);
+			Platform.runLater(() -> paneFXid.getChildren().add(imageView));
+			player1.receivePlate(0, plate, imageView);
+		}
+		savedPlates = data[1].getlefttHandshapes();
+		for (final SaveShapeNode savedPlate : savedPlates) {
+			final Shape plate = PlatesFactory.getInstance().getShape(savedPlate.getName());
+			final ImageView imageView = convertImage(plate.getImage());
+			imageView.setFitHeight(DimensionsConstants.IMAGE_SIZE);
+            imageView.setFitWidth(DimensionsConstants.IMAGE_SIZE);
+            Platform.runLater(() -> paneFXid.getChildren().add(imageView));
+			player1.receivePlate(1, plate, imageView);
+		}
+		savedPlates = data[1].getRightHandshapes();
+		for (final SaveShapeNode savedPlate : savedPlates) {
+			final Shape plate = PlatesFactory.getInstance().getShape(savedPlate.getName());
+			final ImageView imageView = convertImage(plate.getImage());
+			imageView.setFitHeight(DimensionsConstants.IMAGE_SIZE);
+            imageView.setFitWidth(DimensionsConstants.IMAGE_SIZE);
+            Platform.runLater(() -> paneFXid.getChildren().add(imageView));
+			player2.receivePlate(0, plate, convertImage(plate.getImage()));
+		}
+	}
+
 }
